cb760a6d2506fbb98af101ba067048872ff4ad83
Victor RED <viktor_RED_rudko@mail.ru>
2024-04-03 15:16:44 +0300

    feat: style Slogan section

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,12 +1,19 @@
 import styled from "styled-components"
 import { SectionTitle } from "../../../components/SectionTitle"
 import { Button } from "../../../components/Button"
+import { Container } from "../../../components/Container"
+import { FlexWrapper } from "../../../components/FlexWrapper"
+import { Theme } from "../../../styles/Theme"
 
 export const Slogan = () => {
   return (
     <StyledSlogan>
-      <SectionTitle>Enjoy every moment!!!</SectionTitle>
-      <Button>Hire me</Button>
+      <Container>
+        <FlexWrapper direction="column" align="center">
+          <SectionTitle>Enjoy every moment!!!</SectionTitle>
+          <Button>Hire me</Button>
+        </FlexWrapper>
+      </Container>
     </StyledSlogan>
   )
 }
@@ -14,4 +21,15 @@ export const Slogan = () => {
 const StyledSlogan = styled.section`
   background-color: #5f3b3b;
   min-height: 20vh;
+
+  ${Button} {
+    background-color: ${Theme.colors.noAccent};
+    color: ${Theme.colors.mainBg};
+    border-radius: 15px;
+    transition: all 0.5s;
+
+    &:hover {
+      background-color: ${Theme.colors.font};
+    }
+  }
 `
